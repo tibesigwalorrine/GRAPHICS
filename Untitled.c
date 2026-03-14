745e52a444bc04f88937e0b8b4520de2e7911b3d
tibesigwa <tibesigwalorrine@gmail.com>
2026-03-14 09:12:24 -0700

    Initial commit

---
+++ Untitled.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#define pi 3.142
+main()
+{
+int area,circumference,radius;
+printf("enter the area of a circle:\n");
+scanf("%d",&area);
+
+printf("enter the circumference of a circle:\n");
+scanf("%d",&circumference);
+
+printf("enter the radius of a circle:\n");
+scanf("%d",&radius);
+
+area=pi*r*r;
+circumference=2*pi*r;
+radius=circumference/2*pi;
+
+printf("the area of the circle is: %d\n",area);
+printf("circumference of a circle is:%d\n",circumference);
+printf("radius of a circle is :%d\n",radius);
+
+
+return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
